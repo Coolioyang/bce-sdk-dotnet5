06442bebbfa833505ab75b364c4652c009f78bde
coolio <coolio@hotmail.com>
2021-05-22 23:59:33 +0800

    修改Readme

---
+++ BceSdkDotNet/Services/SMS/SMSBceClientConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BaiduBce.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiduBce.Services.SMS
+{
+    public class SMSBceClientConfiguration: BceClientConfiguration
+    {
+        public SMSBceClientConfiguration(string accessKeyId, string secretKey)
+        {
+            Credentials = new DefaultBceCredentials(accessKeyId, secretKey);
+            Endpoint = "https://smsv3.bj.baidubce.com";
+            SignOptions.HeadersToSign.Add("host");
+            SignOptions.HeadersToSign.Add("x-bce-date");
+        }
+    }
+}
--- sms_sample/Program.cs
+++ sms_sample/Program.cs
@@ -10,21 +10,14 @@ namespace sms_sample
     {
         static void Main(string[] args)
         {
-            
 
             const string accessKeyId = "AAAA"; // 用户的Access Key ID
             const string secretAccessKey = "BBBB"; // 用户的Secret Access Key
 
+            var config = new SMSBceClientConfiguration(accessKeyId, secretAccessKey);
+            var client = new SMSClient(config);
 
-            BceClientConfiguration config = new BceClientConfiguration();
-            config.Credentials = new DefaultBceCredentials(accessKeyId, secretAccessKey);
-            config.Endpoint = "https://smsv3.bj.baidubce.com";
-            config.SignOptions.HeadersToSign.Add("host");
-            config.SignOptions.HeadersToSign.Add("x-bce-date");
-
-            SMSClient client = new SMSClient(config);
-
-            SendSMSRequest req = new SendSMSRequest();
+            var req = new SendSMSRequest();
             req.Mobile = "13xxxxxxx";
             req.SignatureId = "sms-sign-xxxx";
             req.Template = "sms-tmpl-xxxxx";
